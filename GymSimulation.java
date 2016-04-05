137e7815831d31fe59d7759f186c3c3be6d62fa7
znaeb <urnotbeanz@gmail.com>
2016-04-05 18:20:15 -0400

    boop
    fnargle

--- GymSimulation.java
+++ GymSimulation.java
@@ -14,11 +14,8 @@ public class GymSimulation {
         while(programLoop){
             System.out.println("\nChoose an option ");
             System.out.println("0: Exit program");
-            System.out.println("1: Show list of direct routes, distances and prices");
-            System.out.println("2: Display a minimum spanning tree based on route distances");
-            System.out.println("3: Display the path with the shortest distance between two cities");
-            System.out.println("4: Display the path with the lowest cost between two cities");
-            System.out.println("5: Display the path with least stops between two cities");
+            System.out.println("1: ");
+            System.out.println("2: ");
             int optionChoice = scan.nextInt();
             //fromUser=fromUser.trim();
             if (optionChoice==0){
